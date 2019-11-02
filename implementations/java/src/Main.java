533669c38fd75197d878037801943af47497c658
Nathan Fritzler <nfblaster@live.com>
2019-11-01 21:35:54 -0600

    Test Java's command line options

--- implementations/java/src/Main.java
+++ implementations/java/src/Main.java
@@ -2,7 +2,14 @@ public class Main {
     private static int[] version = {0,0,0};
     public static void main(String[] args) {
         if (args.length==0) helpText();
+        else{
+            System.out.print("Arg "+args[0]);
+        }
     }
+
+    /**
+     * Print help text
+     */
     private static void helpText() {
         System.out.printf("jchamlc version %d.%d.%d\n\n",version[0],version[1],version[2]);
         System.out.println("No help text yet. Why not add some; this is open source!");
