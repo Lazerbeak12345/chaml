be6a1fa471b3c9ffb2e71ffb5d690044fb8e67ed
Nathan Fritzler <nfblaster@live.com>
2019-11-01 20:35:37 -0600

    Add help text

--- implementations/java/src/Main.java
+++ implementations/java/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+    private static int[] version = {0,0,0};
     public static void main(String[] args) {
-        System.out.print("Okay, I guess it might work.");
+        if (args.length==0) helpText();
+    }
+    private static void helpText() {
+        System.out.printf("jchamlc version %d.%d.%d\n\n",version[0],version[1],version[2]);
+        System.out.println("No help text yet. Why not add some; this is open source!");
     }
 }
