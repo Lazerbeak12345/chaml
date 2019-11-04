849a5f3189a0ecd0135624d762fca6a1418a283d
Nathan Fritzler <nfblaster@live.com>
2019-11-03 22:16:23 -0700

    Added java test script
    Also changed around some java text formatting

---
+++ implementations/java/jchamlc.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/jdk-12.0.2/bin/java -Dfile.encoding=UTF-8 -cp /home/nate/.config/Code/User/workspaceStorage/6440e3e5d2ac499fdd953fc07d5818dd/redhat.java/jdt_ws/jdt.ls-java-project/bin Main $1
--- implementations/java/src/Main.java
+++ implementations/java/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         if (args.length==0) helpText();
         else{
-            System.out.print("Arg "+args[0]);
+            System.out.println(args[0]);
         }
     }
 
