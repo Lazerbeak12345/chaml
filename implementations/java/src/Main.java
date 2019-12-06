7aaac35692218b69c393af1b9fbbda9aeba3bd57
Nathan Fritzler <nfblaster@live.com>
2019-12-06 16:33:26 -0700

    Reflect changes on some test code
    also make it test that code

--- implementations/java/src/Main.java
+++ implementations/java/src/Main.java
@@ -1,12 +1,14 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
 
 public class Main {
-	private static int[] version = {0,0,0};
+	//private static int[] version = {0,0,0};
 	public static void main(String[] args) {
-		if (args.length==0) helpText();
-		else{
-			String filename=args[0];
+		//if (args.length==0) helpText();
+		//else{
+			String[]a={"tests/testLib.chaml"};
+			Tokeniser.main(a);
+			/*String filename=args[0];
 			try{
 				Tokeniser tok=new Tokeniser(filename);
 				System.out.println(tok.read());
@@ -15,16 +17,16 @@ public class Main {
 			}catch(FileNotFoundException e) {
 				System.out.printf("Couldn't find file \"%s\".\n",filename);
 			}catch(IOException e) {
-				System.out.printf("IOExeption with file \"%s\".",filename);
-			}
-		}
+				System.out.printf("IOException with file \"%s\".",filename);
+			}*/
+		//}
 	}
 
 	/**
 	 * Print help text
-	 */
+	 *
 	private static void helpText() {
 		System.out.printf("jchamlc version %d.%d.%d\n\n",version[0],version[1],version[2]);
 		System.out.println("No help text yet. Why not add some; this is open source!");
-	}
+	}//*/
 }
