1ac68c5d7656daaab6bcbe404bee49f555745fee
Nathan Fritzler <nfblaster@live.com>
2020-01-03 21:32:57 -0700

    Clean up the parser as I have it right now.

--- implementations/java/src/Parser.java
+++ implementations/java/src/Parser.java
@@ -48,6 +48,7 @@ class Parser {
 		}
 	}
 
+	Tokeniser tr;
 	/**
 	 * Close the input stream.
 	 * @throws IOException If an I/O error occurs
@@ -56,142 +57,32 @@ class Parser {
 		tr.close();
 	}
 
-	Tokeniser tr;
-
 	public Parser(String fileName) throws FileNotFoundException {
 		tr = new Tokeniser(fileName);
 		init();
 	}
-
 	public Parser(File file) throws FileNotFoundException {
 		tr = new Tokeniser(file);
 		init();
 	}
-
 	public Parser(FileDescriptor fd) {
 		tr = new Tokeniser(fd);
 		init();
 	}
-
 	public Parser(String fileName, Charset charset) throws IOException {
 		tr = new Tokeniser(fileName, charset);
 		init();
 	}
-
 	public Parser(File file, Charset charset) throws IOException {
 		tr = new Tokeniser(file, charset);
 		init();
 	}
-	private ArrayList<ArrayList<Number>> parseTransforms;
-	private ArrayList<Number> parseNames;
+
 	/**
 	 * The one place to do 90% of constructor related stuff
 	 */
 	private void init() {
 		buffer = new ArrayList<>();
-		/**
-		 * An easy to modify parseLogic. follows this JSON structure:
-		 * 
-		 * [
-		 *   ["name"],
-		 *   ["",  "first","possible"],
-		 *   ["",  "second"],
-		 *   ["another"],
-		 *   ["",  "thing"]
-		 * ]
-		 */
-		final String[][] parseLogic = {
-			{"WS_OR_COMMENT"},
-			//{"",	"whitespace"},
-			{"",	"comment"},
-			//{"",	"multiComment"},
-			//{"",	"WS_OR_COMMENT","WS_OR_COMMENT"},
-			//{"IDENTIFIER_LIST"},// A list of _only_ identifiers (>=1)
-			//{"",	"identifier"}, // Will require LA(1)
-			//{"",	"IDENTIFIER_LIST","comma","identifier"},
-			//{"","IDENTIFIER_LIST","comma","IDENTIFIER_LIST"},
-			//{"STATEMENT"},//Like C, do this to get the reference when it hasn't been defined.
-			//{"INLINE_FUNCTION"},
-			//{"",	"openP","closeP","lambda","STATEMENT"},
-			//{"",	"identifier","lambda","STATEMENT" },
-			//{"",	"openP","IDENTIFIER_LIST","closeP","lambda","STATEMENT"},
-			//{"ROOT"},//Root _must_ be defined, or else it is a parse error
-			//{"MULTILINE_FUNCTION_BODY"},
-			//{"",	"openC","ROOT","closeC"},
-			//{"",	"openC","ROOT","statementSeparator","closeC"},// Optional semicolon
-			//{"MULTILINE_FUNCTION"},
-			//{"",	"openC","ROOT","closeC"},
-			//{"",	"identifier","MULTILINE_FUNCTION_BODY"},
-			//{"",	"openP","IDENTIFIER_LIST","closeP","MULTILINE_FUNCTION_BODY"},
-			//{"FUNCTION"},
-			//{"",	"INLINE_FUNCTION"},
-			//{"",	"MULTILINE_FUNCTION"},
-			//{"EXPRESSION"},
-			//{"VALUE_LIST"},
-			//{"",	"EXPRESSION"},
-			//{"",	"VALUE_LIST","comma","VALUE_LIST"},
-			//{"",	"VALUE_LIST","comma","IDENTIFIER_LIST"},// If there are identifiers mixed in, grab them too.
-			//{"",	"IDENTIFIER_LIST","comma","VALUE_LIST"},
-			//{"FUNCTION_CALL"},
-			//{"",	"openP","VALUE_LIST","closeP"},
-			//{"",	"openP","IDENTIFIER_LIST","closeP"}, // A list of identifiers could still be a list of values. (conflict with MULTILINE FUNCTION[1])
-			//{"EXPRESSION"},
-			//{"",	"string"},
-			//{"",	"char"},
-			//{"",	"number"},
-			//{"",	"syntaxExtension"},
-			//{"",	"FUNCTION"},
-			//{"",	"EXPRESSION","subitem","identifier"},
-			//{"",	"EXPRESSION","FUNCTION_CALL"},
-			//{"",	"identifier","FUNCTION_CALL"},
-			//{"STATEMENT"},
-			//{"",	"import"},
-			//{"",	"EXPRESSION"},
-			//{"",	"WS_OR_COMMENT","STATEMENT"},// before every statement, or collection of statements, allow WS or COMMENT
-			//{"",	"identifier","equals","EXPRESSION" },
-			//{"", "identifier","equals","STATEMENT"},//Two or more vars can share a value
-			//{"ROOT"},// Think of ROOT as a "STATEMENT_LIST"
-			//{"",	"STATEMENT"},
-			//{"",	"ROOT","statementSeparator","STATEMENT"},
-		};
-		parseTransforms=new ArrayList<>();
-		parseNames=new ArrayList<>();
-		/** the name of this reduction */
-		int reductionName = -1;
-		ParseNode.nodes=new ArrayList<>();
-		ParseNode.nodes.add("_leaf_");
-		//iterate through each row
-		for (int i = 0; i < parseLogic.length; ++i) {
-			/** The current row */
-			String[] row = parseLogic[i];
-			if (row.length < 1)//If it's empty, skip it
-				continue;
-			else if (row.length == 1){//if it is just one item, it's a name
-				//transform that name to a parse node number
-				reductionName = ParseNode.nameToInt(row[0]);
-				if(reductionName<0) {
-					ParseNode.nodes.add(row[0]);//Add this as a possible node type
-					reductionName = ParseNode.nameToInt(row[0]);//This also handles for chamlctoks
-				}
-			}else {//otherwise, it's a transform to the last name
-				/** The new row */
-				var newR = new ArrayList<Number>();
-				//iterate on each item in the current row, ignoring the first item (an empty string)
-				for (int j = 1; j < row.length; ++j) {
-					/* transform the name at the given index in this row, `j` 
-					into an int, and insert it into `newR` at that position
-					minus 1, to offset the empty string beginning each line */
-					//newR.set(j-1,ParseNode.nameToInt(row[j]));
-					int num=ParseNode.nameToInt(row[j]);
-					if (num<0) throw new Error("Error encountered while converting parseLogic!\nRow num: "+i+" Col num: "+j+"\nSection:"+ParseNode.intToName(reductionName)+" Node:"+row[j]);
-					newR.add(num);
-				}
-				/*As we are always adding to both at the same time, they are 
-				always going to be the same length*/
-				parseTransforms.add(newR);
-				parseNames.add(reductionName);
-			}
-		}
 	}
 
 	/**
@@ -215,40 +106,7 @@ class Parser {
 	 * @return true when a reduction is made, false if nothing changed
 	 */
 	public boolean reduce(){
-		for(int bufferI=0;bufferI<buffer.size();++bufferI){
-			int largest=-1;
-			for(int parseTransformsI=0;parseTransformsI<parseTransforms.size();++parseTransformsI){
-				var transform=parseTransforms.get(parseTransformsI);
-				boolean doesTransformMatch=true;
-				for(int transformI=0;transformI<transform.size();++transformI){
-					if (bufferI+transformI>=buffer.size()) continue;
-					if (buffer.get(bufferI+transformI).getNumber()!=
-					transform.get(transformI).intValue()){
-						doesTransformMatch=false;
-						break;
-					}
-				}
-				if (!doesTransformMatch) continue;
-				if (largest==-1||transform.size()>parseTransforms.get(largest).size())
-					largest=parseTransformsI;
-			}
-			if (largest==-1) continue;
-			//TODO: items changing orders
-			var transform=parseTransforms.get(largest);
-			var temp=new ArrayList<ParseNode>();
-			for(int transformI=0;
-				transformI<transform.size();
-				++transformI
-			){
-				if (bufferI+transformI>=buffer.size()) break;
-				System.out.println("Removing:"+buffer.get(bufferI+transformI).getAsXML()+" for "+ParseNode.intToName(parseNames.get(largest).intValue()));
-				temp.add(buffer.remove(bufferI+transformI));
-			}
-			buffer.add(0,new ParseTree(ParseNode.intToName(parseNames.get(largest).intValue()),temp));
-			//buffer.add(new ParseTree(ParseNode.intToName(parseNames.get(largest).intValue()),temp));
-			return true;
-		}
-		return false;
+		return false;//TODO: dummy function
 	}
 	
 	/**
